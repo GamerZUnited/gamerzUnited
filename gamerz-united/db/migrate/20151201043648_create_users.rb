af0121464872bd6da1de55ab8ab2376cfbde989a
Darryl Cooper <darryl.cooper12@gmail.com>
2015-12-01 00:31:07 -0500

    migrations and schema generated

---
+++ gamerz-united/db/migrate/20151201043648_create_users.rb
@@ -0,0 +1,21 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.string :gamertag, null: false
+      t.string :email, null: false
+      t.string :password_digest
+      t.string :access_token
+      t.integer :age
+      t.string :sex
+      t.attachment :avatar
+
+      t.timestamps null: false
+    end
+  end
+
+  def self.down
+    remove_attachment :users, :avatar
+  end
+end
