fa1d84909954fa35e5b7caa634554a070da6837b
Darryl Cooper <darryl.cooper12@gmail.com>
2015-12-01 16:23:34 -0500

    edit controllers for json views

--- gamerz-united/app/controllers/posts_controller.rb
+++ gamerz-united/app/controllers/posts_controller.rb
@@ -36,6 +36,6 @@ class PostsController < ApplicationController
     
     private
     def post_params
-      params.require(:post).permit(:title, :tag_names, :content)
+      params.require(:post).permit(:title, :content)
     end
 end
